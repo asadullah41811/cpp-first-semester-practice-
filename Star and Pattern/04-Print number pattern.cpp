4a764265c7f0cc3631fa2e4ade11da5bcd3fdd91
Asadullah <asadullahyounas1437@gmail.com>
2026-03-11 00:06:34 +0500

    uploaded the number pattern

---
+++ Star and Pattern/04-Print number pattern.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+ int main()
+ {
+ 	int n;
+ 	cout<<" Enter the row of the pattern::"<<endl;
+ 	cin>>n;
+ 	cout<<" Number pattern row is ::"<< n <<endl; 
+ 	 for( int i=1; i<=n;i++)
+ 	 {
+ 	 	for(int  j=1;j<=i;j++)
+ 	 	{
+ 	 		cout<<i<<" ";
+ 	 		
+		  }
+		  cout<<"\n";
+	  }
+ return 0;
+ }
+
